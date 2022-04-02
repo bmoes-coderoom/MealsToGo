c3c88e00cc839278a50be6c3a1df77ee2a8f2d35
Bmoe Coderoom <Bmoe.Coderoom@example.com>
2022-04-02 13:07:31 -0700

    Implemented FlatList on RestaurantsScreen component using hardcoded data

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import styled from 'styled-components/native';
-import {
-    StatusBar,
-    SafeAreaView,
-    View,
-  } from "react-native";
-  import { Searchbar } from "react-native-paper";
-  import { RestaurantInfoCard } from '../components/restaurant-info-card.component';
+import { StatusBar, FlatList, SafeAreaView, View } from "react-native";
+import { Searchbar } from "react-native-paper";
+import { RestaurantInfoCard } from '../components/restaurant-info-card.component';
+import { Spacer } from "../../../components/spacer/spacer.component";
   
 const SafeArea = styled(SafeAreaView)`
           flex: 1;
@@ -30,9 +27,31 @@ export const RestaurantsScreen = () => {
       <SearchContainer>
         <Searchbar/>
       </SearchContainer>
-      <RestaurantListContainer>
-        <RestaurantInfoCard/>
-      </RestaurantListContainer>
+      <FlatList
+      data={[
+        { name: 1 },
+        { name: 2 },
+        { name: 3 },
+        { name: 4 },
+        { name: 5 },
+        { name: 6 },
+        { name: 7 },
+        { name: 8 },
+        { name: 9 },
+        { name: 10 },
+        { name: 11 },
+        { name: 12 },
+        { name: 13 },
+        { name: 14 },
+      ]}
+      renderItem={() => (
+        <Spacer position="bottom" size="large">
+          <RestaurantInfoCard />
+        </Spacer>
+      )}
+      keyExtractor={(item) => item.name}
+      contentContainerStyle={{ padding: 16 }}
+    />
     </SafeArea>
 
     );
