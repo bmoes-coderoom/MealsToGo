b69f7b49aea611a5b85e9262da50a68258f560a0
Bmoe Coderoom <Bmoe.Coderoom@example.com>
2022-04-03 12:26:20 -0700

    Implemented Tab Navigation. Have three tabs: Restaurants, Map, and Settings

--- App.js
+++ App.js
@@ -8,7 +8,23 @@ import {
 } from "@expo-google-fonts/oswald";
 import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 import { theme } from "./src/infrastructure/theme";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Text } from 'react-native';
+import { SafeArea } from './src/components/utility/safe-area.component';
 
+const Settings = () => (
+  <SafeArea>
+    <Text>Settings</Text>
+  </SafeArea>
+);
+const Map = () => (
+  <SafeArea>
+    <Text>Map</Text>
+  </SafeArea>
+);
+
+const Tab = createBottomTabNavigator();
 export default function App() {
   const [oswaldLoaded] = useOswald({
     Oswald_400Regular,
@@ -26,7 +42,13 @@ export default function App() {
   return (
     <>
     <ThemeProvider theme={theme}>
-    <RestaurantsScreen/>
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
+          <Tab.Screen name="Map" component={Map} />
+          <Tab.Screen name="Settings" component={Settings} />
+        </Tab.Navigator>
+      </NavigationContainer>
       <ExpoStatusBar style="auto" />
     </ThemeProvider>
 
