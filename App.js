0c8fbe423c6185f066b6054afc09df5be637cb5f
Bmoe Coderoom <Bmoe.Coderoom@example.com>
2022-04-04 04:32:24 -0700

    Added fake api request to restaurants.service.js

--- App.js
+++ App.js
@@ -13,6 +13,7 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Text } from 'react-native';
 import { SafeArea } from './src/components/utility/safe-area.component';
 import { Ionicons } from "@expo/vector-icons";
+import { restaurantsRequest } from "./src/services/restaurants/restaurants.service"
 
 const Settings = () => (
   <SafeArea>
