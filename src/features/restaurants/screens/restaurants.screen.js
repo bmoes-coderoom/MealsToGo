8d257a1697dc0e5b1637ac99976206b9da1b618c
Bmoe Coderoom <Bmoe.Coderoom@example.com>
2022-04-04 06:56:28 -0700

    Set up RestaurantContext with dummy data

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import styled from 'styled-components/native';
 import { FlatList, View} from "react-native";
 import { Searchbar } from "react-native-paper";
 import { RestaurantInfoCard } from '../components/restaurant-info-card.component';
 import { Spacer } from "../../../components/spacer/spacer.component";
 import { SafeArea } from '../../../components/utility/safe-area.component';
+import { RestaurantsContext } from '../../../services/restaurants/restaurants.context';
 
 const SearchContainer = styled(View)`
           padding: ${(props) => props.theme.space[3]};
@@ -24,37 +25,22 @@ const RestaurantListContainer = styled(View)`
 
 
 export const RestaurantsScreen = () => {
-    return (
-      <SafeArea>
+  const restaurantContext = useContext(RestaurantsContext);
+  console.log(restaurantContext)
+  return (
+    <SafeArea>
       <SearchContainer>
-        <Searchbar/>
+        <Searchbar />
       </SearchContainer>
       <RestaurantList
-      data={[
-        { name: 1 },
-        { name: 2 },
-        { name: 3 },
-        { name: 4 },
-        { name: 5 },
-        { name: 6 },
-        { name: 7 },
-        { name: 8 },
-        { name: 9 },
-        { name: 10 },
-        { name: 11 },
-        { name: 12 },
-        { name: 13 },
-        { name: 14 },
-      ]}
-      renderItem={() => (
-        <Spacer position="bottom" size="large">
-          <RestaurantInfoCard />
-        </Spacer>
-      )}
-      keyExtractor={(item) => item.name}
-    />
+        data={restaurantContext.restaurants}
+        renderItem={() => (
+          <Spacer position="bottom" size="large">
+            <RestaurantInfoCard />
+          </Spacer>
+        )}
+        keyExtractor={(item) => item.name}
+      />
     </SafeArea>
-
-    );
-
-}
+  );
+};
