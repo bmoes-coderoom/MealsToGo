7afc35b13d01ca30d8b36601f6c53bfa56de9411
Bmoe Coderoom <Bmoe.Coderoom@example.com>
2022-04-04 07:39:40 -0700

    Implemented ActivityIndicator inside RestaurantsScreen

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,14 +1,17 @@
-import React, {useContext} from 'react';
-import styled from 'styled-components/native';
-import { FlatList, View} from "react-native";
+import React, { useContext } from "react";
 import { Searchbar } from "react-native-paper";
-import { RestaurantInfoCard } from '../components/restaurant-info-card.component';
-import { Spacer } from "../../../components/spacer/spacer.component";
-import { SafeArea } from '../../../components/utility/safe-area.component';
-import { RestaurantsContext } from '../../../services/restaurants/restaurants.context';
+import { FlatList } from "react-native";
+import styled from "styled-components/native";
+import { ActivityIndicator, Colors } from "react-native-paper";
 
-const SearchContainer = styled(View)`
-          padding: ${(props) => props.theme.space[3]};
+import { SafeArea } from "../../../components/utility/safe-area.component";
+import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
+import { Spacer } from "../../../components/spacer/spacer.component";
+
+import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
+
+const SearchContainer = styled.View`
+  padding: ${(props) => props.theme.space[3]};
 `;
 
 const RestaurantList = styled(FlatList).attrs({
@@ -17,18 +20,24 @@ const RestaurantList = styled(FlatList).attrs({
   },
 })``;
 
-const RestaurantListContainer = styled(View)`
-          flex: 1;
-          padding: ${(props) => props.theme.space[3]};
-
+const Loading = styled(ActivityIndicator)`
+  margin-left: -25px;
 `;
-
+const LoadingContainer = styled.View`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+`;
 
 export const RestaurantsScreen = () => {
   const { isLoading, error, restaurants } = useContext(RestaurantsContext);
-  console.log(error);
   return (
     <SafeArea>
+      {isLoading && (
+        <LoadingContainer>
+          <Loading size={50} animating={true} color={Colors.blue300} />
+        </LoadingContainer>
+      )}
       <SearchContainer>
         <Searchbar />
       </SearchContainer>
