12c886e1a62717eee46c148bddc0f1023a4b0102
Bmoe Coderoom <Bmoe.Coderoom@example.com>
2022-04-05 11:32:42 -0700

    Tied up some loose ends with Login screen -Added Back button on screen -Added title on screen -Added custom style for Error message on screen

--- src/features/account/screens/login.screen.js
+++ src/features/account/screens/login.screen.js
@@ -5,18 +5,21 @@ import {
   AccountContainer,
   AuthButton,
   AuthInput,
+  ErrorContainer,
+  Title,
 } from "../components/account.styles";
 import { Text } from "../../../components/typography/text.component";
 import { Spacer } from "../../../components/spacer/spacer.component";
 import { AuthenticationContext } from "../../../services/authentication/authentication.context";
 
-export const LoginScreen = () => {
+export const LoginScreen = ({navigation}) => {
     const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const { onLogin, error } = useContext(AuthenticationContext);
   return (
     <AccountBackground>
       <AccountCover />
+      <Title>Meals To Go</Title>
       <AccountContainer>
         <AuthInput
           label="E-mail"
@@ -26,7 +29,7 @@ export const LoginScreen = () => {
           autoCapitalize="none"
           onChangeText={(u) => setEmail(u)}
         />
-        <Spacer size="large">
+        <ErrorContainer size="large">
           <AuthInput
             label="Password"
             value={password}
@@ -36,7 +39,7 @@ export const LoginScreen = () => {
             secure
             onChangeText={(p) => setPassword(p)}
           />
-        </Spacer>
+        </ErrorContainer>
         {error && (
           <Spacer size="large">
             <Text variant="error">{error}</Text>
@@ -52,6 +55,11 @@ export const LoginScreen = () => {
           </AuthButton>
         </Spacer>
       </AccountContainer>
+      <Spacer size="large">
+        <AuthButton mode="contained" onPress={() => navigation.goBack()}>
+          Back
+        </AuthButton>
+      </Spacer>
     </AccountBackground>
   );
 };
