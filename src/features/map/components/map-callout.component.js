a1391e8d12e768a9c647ef4dc09ceca3d9ab7caf
Bmoe Coderoom <Bmoe.Coderoom@example.com>
2022-04-05 09:02:26 -0700

    Implement persisting favourites

--- src/features/map/components/map-callout.component.js
+++ src/features/map/components/map-callout.component.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import { Text, View, Image } from 'react-native';
 import { CompactRestaurantInfo } from "../../../components/restaurant/compact-restaurant-info.component";
 export const MapCallout = ({ restaurant }) => {
     return (
-        <CompactRestaurantInfo restaurant={restaurant} />
+        <CompactRestaurantInfo isMap restaurant={restaurant} />
     );
 
 };
